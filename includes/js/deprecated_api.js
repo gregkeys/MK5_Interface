5719ad3f12aaf7a7b574f9341e8dd09b8b5c8c6f
Greg Keys <gregkeys+github@gmail.com>
2014-12-20 02:51:52 -0800

    initial commit of deprecated api

---
+++ includes/js/deprecated_api.js
@@ -0,0 +1,49 @@
+/**
+ * Created by leathalinjexion on 12/20/14.
+ */
+
+
+//handle deprecated global api
+function notify(message, sender, color) {
+    Pineapple.notify(message, sender, color);
+}
+
+function draw_small_tile(name, type) {
+    Pineapple.draw_small_tile(name, type);
+}
+
+function hide_small_tile(name, persistent) {
+    Pineapple.hide_small_tile(name, persistent);
+}
+
+function add_hidden_tile(tile) {
+    Pineapple.add_hidden_tile(tile);
+}
+
+function update_tile(name, type, data) {
+    Pineapple.update_tile(name, type, data);
+}
+
+function refresh_small(name, type) {
+    Pineapple.refresh_small(name, type);
+}
+
+function popup(message) {
+    Pineapple.popup(message);
+}
+
+function close_popup() {
+    Pineapple.close_popup();
+}
+
+function draw_large_tile(name, type, data) {
+    Pineapple.draw_large_tile(name, type, data);
+}
+
+function hide_large_tile() {
+    Pineapple.hide_large_tile();
+}
+
+function handle_hash_change(hashValue) {
+    Pineapple.handle_hash_change(hashValue);
+}
